5ccaadcee7b827d7bbcbe83b187ff9c590099cfb
MANIKUMAR M <136432209+manikumar50@users.noreply.github.com>
2024-11-21 18:31:13 +0530

    !fix : the method is not needed here !

--- src/main/java/com/azh/AZH/Shop/controller/MainController.java
+++ src/main/java/com/azh/AZH/Shop/controller/MainController.java
@@ -6,11 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController
 {
-    @GetMapping("/home")
-    public String Hello()
-    {
-        return "Say Hello";
-    }
+   
     @GetMapping("/getAny")
     public String getIdByName()
     {
