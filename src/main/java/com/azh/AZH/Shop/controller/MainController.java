5e3dd54a48c6e35a8b42830680865245ec29c46a
MANIKUMAR M <136432209+manikumar50@users.noreply.github.com>
2024-11-21 18:23:13 +0530

    refactor : changed end point

--- src/main/java/com/azh/AZH/Shop/controller/MainController.java
+++ src/main/java/com/azh/AZH/Shop/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController
 {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String Hello()
     {
         return "Say Hello";
